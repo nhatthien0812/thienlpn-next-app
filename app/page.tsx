0835ac32e1fa0f6c4f827e03bb2f09436530e63c
thienlpn <thienlpn0812@gmail.com>
2023-09-26 00:44:30 +0700

    update: add prefix asset

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main>
       <section className="max-w-7xl mx-auto min-h-screen relative">
-        <h1>Front-end Mentor</h1>
-        <div className="absolute flex space-x-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <h1 className="font-bold mb-4">Front-end Mentor</h1>
           <Link href="/order-summary-component-main">
             <button className="p-4 mx-auto w-full bg-green-400 text-lg rounded-lg hover:brightness-75">
               Order Summary
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/thienlpn-next-app' : undefined,
+  assetPrefix: isProd ? '/thienlpn-next-app' : undefined,
 }
 
 module.exports = nextConfig
