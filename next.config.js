e23b90987cf6c54b7821b92de185fd433b5c51f8
thienlpn <thienlpn0812@gmail.com>
2023-09-26 11:13:10 +0700

    rebuild

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/thienlpn-next-app' : undefined,
-  basePath: isProd ? '/thienlpn-next-app' : undefined,
+  basePath: '/thienlpn-next-app',
 }
 
 module.exports = nextConfig
